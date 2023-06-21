0ad950b5cc08d9273f921b5cb251cc37db4296f8
AaraoAbbate <134648305+AaraoAbbate@users.noreply.github.com>
2023-06-21 17:18:45 -0300

    AtividadeDojao

---
+++ AppCalculodeMedia/AppCalculodeMedia/FrmCalculomedia.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AppCalculodeMedia
+{
+    partial class FrmCalculoMédia
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblNota1 = new System.Windows.Forms.Label();
+            this.TxtNota1 = new System.Windows.Forms.TextBox();
+            this.LblNota2 = new System.Windows.Forms.Label();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.LblNota3 = new System.Windows.Forms.Label();
+            this.LblNota4 = new System.Windows.Forms.Label();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.LblMédia = new System.Windows.Forms.Label();
+            this.LblSituacao = new System.Windows.Forms.Label();
+            this.LblResultadoMedia = new System.Windows.Forms.Label();
+            this.LblResultadoMédia = new System.Windows.Forms.Label();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblNota1
+            // 
+            this.LblNota1.AutoSize = true;
+            this.LblNota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNota1.Location = new System.Drawing.Point(12, 21);
+            this.LblNota1.Name = "LblNota1";
+            this.LblNota1.Size = new System.Drawing.Size(70, 24);
+            this.LblNota1.TabIndex = 0;
+            this.LblNota1.Text = "Nota 1";
+            // 
+            // TxtNota1
+            // 
+            this.TxtNota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNota1.Location = new System.Drawing.Point(10, 48);
+            this.TxtNota1.Name = "TxtNota1";
+            this.TxtNota1.Size = new System.Drawing.Size(256, 29);
+            this.TxtNota1.TabIndex = 1;
+            // 
+            // LblNota2
+            // 
+            this.LblNota2.AutoSize = true;
+            this.LblNota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNota2.Location = new System.Drawing.Point(6, 80);
+            this.LblNota2.Name = "LblNota2";
+            this.LblNota2.Size = new System.Drawing.Size(70, 24);
+            this.LblNota2.TabIndex = 2;
+            this.LblNota2.Text = "Nota 2";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota2.Location = new System.Drawing.Point(10, 107);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(256, 29);
+            this.txtNota2.TabIndex = 3;
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota3.Location = new System.Drawing.Point(10, 186);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(256, 29);
+            this.txtNota3.TabIndex = 4;
+            // 
+            // LblNota3
+            // 
+            this.LblNota3.AutoSize = true;
+            this.LblNota3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNota3.Location = new System.Drawing.Point(6, 159);
+            this.LblNota3.Name = "LblNota3";
+            this.LblNota3.Size = new System.Drawing.Size(70, 24);
+            this.LblNota3.TabIndex = 5;
+            this.LblNota3.Text = "Nota 3";
+            // 
+            // LblNota4
+            // 
+            this.LblNota4.AutoSize = true;
+            this.LblNota4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNota4.Location = new System.Drawing.Point(6, 228);
+            this.LblNota4.Name = "LblNota4";
+            this.LblNota4.Size = new System.Drawing.Size(70, 24);
+            this.LblNota4.TabIndex = 6;
+            this.LblNota4.Text = "Nota 4";
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNota4.Location = new System.Drawing.Point(10, 255);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(256, 29);
+            this.txtNota4.TabIndex = 7;
+            // 
+            // LblMédia
+            // 
+            this.LblMédia.AutoSize = true;
+            this.LblMédia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMédia.Location = new System.Drawing.Point(385, 9);
+            this.LblMédia.Name = "LblMédia";
+            this.LblMédia.Size = new System.Drawing.Size(76, 25);
+            this.LblMédia.TabIndex = 8;
+            this.LblMédia.Text = "Média";
+            // 
+            // LblSituacao
+            // 
+            this.LblSituacao.AutoSize = true;
+            this.LblSituacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSituacao.Location = new System.Drawing.Point(326, 163);
+            this.LblSituacao.Name = "LblSituacao";
+            this.LblSituacao.Size = new System.Drawing.Size(85, 20);
+            this.LblSituacao.TabIndex = 9;
+            this.LblSituacao.Text = "Situação:";
+            // 
+            // LblResultadoMedia
+            // 
+            this.LblResultadoMedia.AutoSize = true;
+            this.LblResultadoMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultadoMedia.Location = new System.Drawing.Point(410, 163);
+            this.LblResultadoMedia.Name = "LblResultadoMedia";
+            this.LblResultadoMedia.Size = new System.Drawing.Size(85, 20);
+            this.LblResultadoMedia.TabIndex = 10;
+            this.LblResultadoMedia.Text = "Aprovado";
+            // 
+            // LblResultadoMédia
+            // 
+            this.LblResultadoMédia.AutoSize = true;
+            this.LblResultadoMédia.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblResultadoMédia.Location = new System.Drawing.Point(383, 48);
+            this.LblResultadoMédia.Name = "LblResultadoMédia";
+            this.LblResultadoMédia.Size = new System.Drawing.Size(99, 108);
+            this.LblResultadoMédia.TabIndex = 11;
+            this.LblResultadoMédia.Text = "7";
+
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Location = new System.Drawing.Point(330, 197);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(165, 90);
+            this.BtnCalcular.TabIndex = 12;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // FrmCalculoMédia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 450);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.LblResultadoMédia);
+            this.Controls.Add(this.LblResultadoMedia);
+            this.Controls.Add(this.LblSituacao);
+            this.Controls.Add(this.LblMédia);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.LblNota4);
+            this.Controls.Add(this.LblNota3);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.LblNota2);
+            this.Controls.Add(this.TxtNota1);
+            this.Controls.Add(this.LblNota1);
+            this.Name = "FrmCalculoMédia";
+            this.Text = "Calculo de média";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblNota1;
+        private System.Windows.Forms.TextBox TxtNota1;
+        private System.Windows.Forms.Label LblNota2;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label LblNota3;
+        private System.Windows.Forms.Label LblNota4;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Label LblMédia;
+        private System.Windows.Forms.Label LblSituacao;
+        private System.Windows.Forms.Label LblResultadoMedia;
+        private System.Windows.Forms.Label LblResultadoMédia;
+        private System.Windows.Forms.Button BtnCalcular;
+    }
+}
+
---
+++ AtividadeDoJao/AtividadeDoJao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+
+namespace ExemploInterface
+{
+    public interface Ianimal
+    {
+        void EmiteSom();
+    }
+    public class Cachorro : Ianimal
+    {
+        public void EmiteSom()
+        {
+            Console.WriteLine("Chachorro late. ");
+        }
+    }
+
+
+
+    public class Gato : Ianimal
+    {
+        public void EmiteSom()
+        {
+            Console.WriteLine("Gato mia. ");
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Ianimal cachorro1 = new Cachorro();
+            Ianimal gato1 = new Gato();
+
+
+
+            cachorro1.EmiteSom();
+            gato1.EmiteSom();
+
+            Console.ReadLine();
+        }
+    }
+}
